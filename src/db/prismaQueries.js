7a0163765fa5312491d74e9d8acc4478f7bc10a6
danishkodemonkey <humberjini@hotmail.com>
2024-09-02 15:47:20 +0200

    prisma queries getuserbyid getuserbyusername

--- src/db/prismaQueries.js
+++ src/db/prismaQueries.js
@@ -1,5 +1,36 @@
 const prisma = require('./prismaClient');
 
 const userQueries = {
-    getUserById: async (userId) => {},
+    getUserById: async (userId) => {
+        try {
+            const user = await prisma.user.findUnique({
+                where: { id: userId },
+            });
+
+            if (!user) {
+                throw new Error(`User with ID ${userId} not found.`);
+            }
+            return user;
+        } catch (err) {
+            console.error('Error retrieving user by ID: ', err);
+            throw new Error('Error retrieving user by ID');
+        }
+    },
+    getUserByUsername: async (username) => {
+        try {
+            const user = await prisma.user.findUnique({
+                where: { username: username },
+            });
+
+            if (!user) {
+                throw new Error(`User with username ${username} not found.`);
+            }
+            return user;
+        } catch (err) {
+            console.error('Error retrieving user by name: ', err);
+            throw new Error('Error retrieving user by username');
+        }
+    },
 };
+
+module.exports = { userQueries };
