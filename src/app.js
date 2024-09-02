58c4c72bfd54c2f891fa766f951f10a49a40870e
danishkodemonkey <humberjini@hotmail.com>
2024-09-02 15:54:43 +0200

    app.js catchup, implement passport, and session handler to middleware

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
     session({
@@ -19,7 +20,7 @@ app.use(
         saveUninitialized: true,
     })
 );
-
+app.use(passport.session());
 // middleware parses user request object to response locals to let browser carry session along easy
 app.use((req, res, next) => {
     res.locals.currentUser = req.user || null;
@@ -27,6 +28,21 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => res.render('index'));
+
+// Catch all route handler if nothing else matches.
+app.use((req, res, next) => {
+    res.status(404).render('404', { message: 'Page Not Found' });
+});
+
+// Generic error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack); // parse error
+    const statusCode = err.status || 500; // set status code
+
+    res.status(statusCode).json({
+        message: err.message || 'Internal Server Error (500)',
+    });
+});
 
 app.listen(process.env.PORT, () =>
     console.log('App listening on port ', process.env.PORT)
