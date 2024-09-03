b677987e0a21192a6a5e474d45af0c67f0f35d1b
danishkodemonkey <humberjini@hotmail.com>
2024-09-03 15:52:14 +0200

    users signup POST route

--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const user_controller = require('../controllers/userController');
 
-// GET user page
-router.get('/sign-up', user_controller.signUpGet);
+// GET user sign up
+router.get('/sign-up', user_controller.user_signup_get);
+// POST user sign up
+router.post('/sign-up', user_controller.user_signup_post);
 
 module.exports = router;
