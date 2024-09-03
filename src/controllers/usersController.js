ab369b5cd832ec06b45922acb7d1454742c742f1
danishkodemonkey <humberjini@hotmail.com>
2024-09-03 15:56:31 +0200

    User data validation

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,27 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const { body } = require('express-validator');
+const { userQueries } = require('../db/prismaQueries');
 
-exports.signUpGet = asyncHandler(async (req, res) => {
+// Validation for signup
+const validateUser = [
+    body('username')
+        .trim()
+        .escape()
+        .isLength({ min: 1, max: 20 })
+        .withMessage('Username must be between 1 and 20 characters')
+        .isAlphanumeric()
+        .withMessage('Username must be alphanumeric'),
+    body('email')
+        .normalizeEmail()
+        .isEmail()
+        .withMessage('Invalid email address.'),
+    body('password')
+        .isLength({ min: 6, max: 60 })
+        .withMessage('Please keep passwords between 6 and 60 characters'),
+];
+
+exports.user_signup_get = asyncHandler(async (req, res) => {
     res.render('pages/signup', {
         description: 'Sign up page',
         title: 'Sign up',
