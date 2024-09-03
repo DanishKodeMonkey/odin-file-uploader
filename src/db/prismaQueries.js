2f4e5c873b76b74e1baacb64ae09a129d5600032
danishkodemonkey <humberjini@hotmail.com>
2024-09-03 16:13:02 +0200

    prisma getUserByEmail and createUser

--- src/db/prismaQueries.js
+++ src/db/prismaQueries.js
@@ -31,6 +31,36 @@ const userQueries = {
             throw new Error('Error retrieving user by username');
         }
     },
+    getUserByEmail: async (email) => {
+        try {
+            const user = await prisma.user.findFirst({
+                where: { email: email },
+            });
+            return user;
+        } catch (err) {
+            console.error('Error retrieving user by Email: ', err);
+            throw new Error('Error retrieving user by Email');
+        }
+    },
+    createUser: async (userData) => {
+        try {
+            // hash password
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            // create new user
+            const newUser = await prisma.user.create({
+                data: {
+                    username,
+                    email,
+                    password: hashedPassword,
+                },
+            });
+            return newUser;
+        } catch (err) {
+            console.error('Error creating user ', err);
+            throw new Error('Error creating user');
+        }
+    },
 };
 
 module.exports = { userQueries };
