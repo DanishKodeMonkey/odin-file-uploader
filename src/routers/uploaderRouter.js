30b15836505b4d0f0f87c9edf0ec486660618703
danishkodemonkey <humberjini@hotmail.com>
2024-09-05 15:55:27 +0200

    Protect uploader path

--- src/routers/uploaderRouter.js
+++ src/routers/uploaderRouter.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const uploader_controller = require('../controllers/uploaderController');
+const { isAuthenticated } = require('../middleware/authMiddleware'); // Authentication middleware
 
-router.get('/upload', uploader_controller.file_upload_get);
+router.get('/upload', isAuthenticated, uploader_controller.file_upload_get); // protected route
 
-router.post('/upload', uploader_controller.file_upload_post);
+router.post('/upload', isAuthenticated, uploader_controller.file_upload_post); // protected route
 
 module.exports = router;
