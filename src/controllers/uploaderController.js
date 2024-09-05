acfba7f1fafed94b5d6440e60110a8af43fc9a71
danishkodemonkey <humberjini@hotmail.com>
2024-09-05 15:20:14 +0200

    flesh out upload post and configure storage for multer

--- src/controllers/uploaderController.js
+++ src/controllers/uploaderController.js
@@ -1,6 +1,24 @@
 const asyncHandler = require('express-async-handler');
 const multer = require('multer');
-const upload = multer({ dest: '../../uploads/' });
+
+const path = require('path');
+
+// set up multer storage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../../public/uploads')); // Files will be saved here.
+    },
+    filename: function (req, file, cb) {
+        // Create unique filename using timestamps
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 150);
+        cb(null, uniqueSuffix + '-' + file.originalname);
+    },
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1024 * 1024 * 5 }, //5mb size limit
+});
 
 exports.file_upload_get = asyncHandler(async (req, res) => {
     console.log('Hit file upload GET');
@@ -8,9 +26,23 @@ exports.file_upload_get = asyncHandler(async (req, res) => {
         description: 'Upload page',
         title: 'Upload file',
         errors: [],
-        user: {},
+        user: res.locals.currentUser,
     });
 });
 
-(exports.file_upload_post = upload.single('file')),
-    function (req, res, next) {};
+exports.file_upload_post = [
+    upload.single('uploaded_file'),
+    asyncHandler(async (req, res) => {
+        if (!req.file) {
+            return res.status(400).render('pages/upload', {
+                description: 'Upload page',
+                title: 'Upload file',
+                errors: [{ msg: 'Please upload a file' }],
+                user: res.locals.currentUser,
+            });
+        }
+        console.log('File uploaded successfully: ', req.file);
+        console.log('Form data: ', req.body);
+        res.redirect('/');
+    }),
+];
