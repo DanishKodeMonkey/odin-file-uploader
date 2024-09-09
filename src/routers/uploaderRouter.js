1b63b03b552e9fa6be2930284adea7b0d08adff9
danishkodemonkey <humberjini@hotmail.com>
2024-09-09 15:00:50 +0200

    Add folder create POST

--- src/routers/uploaderRouter.js
+++ src/routers/uploaderRouter.js
@@ -4,8 +4,12 @@ const router = express.Router();
 const uploader_controller = require('../controllers/uploaderController');
 const { isAuthenticated } = require('../middleware/authMiddleware'); // Authentication middleware
 
+/* Files */
 router.get('/upload', isAuthenticated, uploader_controller.file_upload_get); // protected route
 
 router.post('/upload', isAuthenticated, uploader_controller.file_upload_post); // protected route
+
+/* Folders */
+router.post('/createFolder', isAuthenticated, uploader_controller.createFolder);
 
 module.exports = router;
