0b3e603010b309032ad52dd73b97573b3599a079
danishkodemonkey <humberjini@hotmail.com>
2024-09-10 15:30:32 +0200

    get folder by ID route

--- src/routers/uploaderRouter.js
+++ src/routers/uploaderRouter.js
@@ -9,11 +9,17 @@ router.get('/upload', isAuthenticated, uploader_controller.file_upload_get); // 
 
 router.post('/upload', isAuthenticated, uploader_controller.file_upload_post); // protected route
 
-// Get all files
-router.get('/files', isAuthenticated, uploader_controller.listFolders);
-
 /* Folders */
+// Get all folders
+router.get('/folders', isAuthenticated, uploader_controller.listFolders);
+
 // Create new folder
 router.post('/createFolder', isAuthenticated, uploader_controller.createFolder);
 
+// Get folder by ID
+router.get(
+    '/folders/:folderId',
+    isAuthenticated,
+    uploader_controller.getFolderById
+);
 module.exports = router;
