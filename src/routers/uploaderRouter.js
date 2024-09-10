e18846b67428c62bdf98a4f07d6765d03766110f
danishkodemonkey <humberjini@hotmail.com>
2024-09-10 16:08:23 +0200

    REFACTOR: authenticate middleware at top of middleware stack of all routes here

--- src/routers/uploaderRouter.js
+++ src/routers/uploaderRouter.js
@@ -4,22 +4,22 @@ const router = express.Router();
 const uploader_controller = require('../controllers/uploaderController');
 const { isAuthenticated } = require('../middleware/authMiddleware'); // Authentication middleware
 
-/* Files */
-router.get('/upload', isAuthenticated, uploader_controller.file_upload_get); // protected route
+// Middleware to ensure user is authenticated before passing request to any routes below
+router.use(isAuthenticated);
 
-router.post('/upload', isAuthenticated, uploader_controller.file_upload_post); // protected route
+/* Files */
+router.get('/upload', uploader_controller.file_upload_get); // protected route
+
+router.post('/upload', uploader_controller.file_upload_post); // protected route
 
 /* Folders */
 // Get all folders
-router.get('/folders', isAuthenticated, uploader_controller.listFolders);
+router.get('/folders', uploader_controller.listFolders);
 
 // Create new folder
-router.post('/createFolder', isAuthenticated, uploader_controller.createFolder);
+router.post('/createFolder', uploader_controller.createFolder);
 
 // Get folder by ID
-router.get(
-    '/folders/:folderId',
-    isAuthenticated,
-    uploader_controller.getFolderById
-);
+router.get('/folders/:folderId', uploader_controller.getFolderById);
+
 module.exports = router;
