377f797192c3d24f3f1bb58bf4c88ecdad340c16
danishkodemonkey <humberjini@hotmail.com>
2024-09-19 15:13:17 +0200

    Add utility helper function to convert bytes to readable unit types (KB, MB, GB)

---
+++ src/utils/helpers.js
@@ -0,0 +1,10 @@
+function formatFileSize(bytes) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return '0 Bytes';
+    // Thanks internet for the math function
+    // Converts bytes to nearest applicable size format, KB, MB, GB etc.
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+}
+
+module.exports = formatFileSize;
